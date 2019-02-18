e8b9dca71beaffec521ddb1b703e7a98cc19a8b2
Jun Cheng <junnobaka@gmail.com>
2019-02-17 23:22:55 -0600

    redid app to use webpack template

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="click('thing')">Button</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  methods: {
+    click: async function(message) {
+      const response = await axios.get('http://httpbin.org/get');
+      alert(JSON.stringify(response));
+    }
   }
 }
 </script>
